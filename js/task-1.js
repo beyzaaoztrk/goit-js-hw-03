35aa20b3015e11b192374871ab45963d4ea70079
beyzaozturkk <beyzaaguneri@gmail.com>
2025-01-20 21:07:00 +0300

    hmmm

--- js/task-1.js
+++ js/task-1.js
@@ -1,15 +1,3 @@
-function makeTransaction (quantity, pricePerDroid, customerCredits) {
-    const totalPrice = quantity * pricePerDroid;
-    if (totalPrice > customerCredits) {
-        return "Insufficient funds!"; 
-    } else {
-        return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-    }
-    }
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
-
 function slugify(title) {
     title = title.toLowerCase();
     let slug = title.split(" ").join("-");
